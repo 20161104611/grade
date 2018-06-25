f37d4656e10ea3c04d006ce1f16adb7b512d1fb1
乔晓波 <1587510366@qq.com>
2018-06-25 11:01:45 +0800

    Initial Commit

---
+++ grade/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grade
+//
+//  Created by s20161104611 on 18/6/25.
+//  Copyright © 2018年 s20161104611. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
